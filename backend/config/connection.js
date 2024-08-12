844036aa32ba20582f6a4a31c5775e85c2c5b8d5
renzitoDev <renson_montoya@soy.sena.edu.co>
2024-08-12 07:50:47 -0500

    update

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const URI = `mongodb+srv://${process.env.USERDB}:${process.env.PASSWORD}@adso2669736.c6a7gzi.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`
+const URI = `mongodb+srv://${process.env.USERDB}:${process.env.PASSWORD}@adso2669736.fa67f9e.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`
 mongoose.connect(URI);
 
 module.exports = mongoose;
